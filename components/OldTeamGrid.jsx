de6a5ea943802d21e67b075ae2885beeb8387516
Luís Espírito Santo <luis.carlos.as@gmail.com>
2024-01-05 01:30:00 +0000

    Functional LinkedIn Urls

---
+++ components/OldTeamGrid.jsx
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+
+import s from '../styles/FoodGrid.module.scss';
+
+function OldTeamGrid({ items }) {
+	const orderedItems = items.map((item) => ({ ...item.frontmatter, ...item })).sort((a, b) => a.slug > b.slug);
+
+	return (
+		<ol className={s.TeamList}>
+			{orderedItems.map(({
+				name, photo, slug, linkedinUrl,
+			}) => (
+				<li className={s.TeamMember} key={slug}>
+					<a href={linkedinUrl}>
+						<img src={photo} alt={name} style={{grayscale: .7}}/>
+					</a>
+				</li>
+			))}
+		</ol>
+	);
+}
+
+OldTeamGrid.defaultProps = {
+	items: [],
+};
+
+OldTeamGrid.propTypes = {
+	items: PropTypes.array,
+};
+
+export default OldTeamGrid;
